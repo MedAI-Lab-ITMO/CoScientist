593712a4ee86d97e248629e482fbd142c0e2e9de
Ivan Gurev <ivanzo17898@gmail.com>
2026-03-30 18:55:33 +0000

    first version

--- CoScientist/__init__.py
+++ CoScientist/__init__.py
@@ -1,1 +1,35 @@
-from CoScientist.config import config as project_config
+"""
+CoScientist Module - A multiagent system for solving scientific tasks.
+"""
+
+from CoScientist.main import (
+    CoScientistManager,
+    create_manager
+)
+
+from CoScientist.tools import FedotMASToolset, WebSearchToolset
+
+from CoScientist.agents import (
+    orchestrator_agent,
+    hypotheses_agent, 
+    research_agent, 
+    fedot_agent
+)
+
+__version__ = "1.0.0"
+
+__all__ = [
+    # Main classes
+    "CoScientistManager",
+    # Models
+    # Tools
+    'FedotMASToolset',
+    'WebSearchToolset',
+    #Agents
+    'orchestrator_agent',
+    'hypotheses_agent',
+    'research_agent',
+    'fedot_agent',
+    # Functions
+    "create_manager"
+]
--- CoScientist/tools/__init__.py
+++ CoScientist/tools/__init__.py
@@ -1,5 +1,6 @@
 """Toolset module."""
-from CoScientist.tools.fedotmas_tools import FedotMASToolset, fedot_toolset_instance, WebSearchToolset, websearch_toolset_instance
+from CoScientist.tools.fedotmas_tools import FedotMASToolset, fedot_toolset_instance
+from CoScientist.tools.web_tools import WebSearchToolset, websearch_toolset_instance
 
 __all__ = ["FedotMASToolset", 
             "fedot_toolset_instance",
